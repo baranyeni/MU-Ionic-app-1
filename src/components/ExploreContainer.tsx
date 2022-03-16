18431632e65330523e425dec9890a6f79cdcc322
Baran Yeni <info@baranyeni.com>
2022-03-16 12:21:11 +0300

    counter feature

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,14 +1,31 @@
 import './ExploreContainer.css';
+import React from "react";
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
+
+    const [counter, setCounter] = React.useState(0)
+
+    function changeCounter(i:number) {
+        setCounter((prevCount) => {
+            if (prevCount === 0 && i < 0) return prevCount;
+            return prevCount + i;
+        })
+    }
+
   return (
     <div className="container">
-        <strong>Ready to becoma a developer?</strong>
-        <p>Here is the story starts..</p>
+        <strong>Counter app</strong>
+        <p>Click on the buttons to set the counter!</p>
+
+        <p id={"counterValue"}> {counter} </p>
+
+        <button className={"counter_buttons"} onClick={() => changeCounter(+1)} > + </button>
+        <button className={"counter_buttons"} onClick={() => changeCounter(-1)} > - </button>
     </div>
   );
 };
+
 
 export default ExploreContainer;
